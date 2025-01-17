e40282fcc9710312301936bce3ee8b9b965e7353
Jahongir <j.jobborov@newuu.uz>
2025-01-17 10:24:31 +0500

    Main updated

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello, how old are you?" << endl;
+    cout << "your age:";
+    cin >> age;
+    cout<<"So, your age:"<<age<<endl;
+    return 0;
+}
